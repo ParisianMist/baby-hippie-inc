6e62cb5ef8e8aebae9338d4c31e7f0ef2445db69
Aunjrya Fleming <104907548+ParisianMist@users.noreply.github.com>
2023-12-23 11:05:44 -0500

    add state to mbl header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,62 @@
+import { useState } from 'react';
+
 import './Header.scss';
 
 import bhLogo from '../../assets/logo/baby-hippie-logo.png';
 import iconMblMenu from '../../assets/icons/icon-header-mbl-menu.svg'
 
+import navLinks from '../../data/services.json'
 const Header = () => {
 
-    //JS Code
+    //state
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen)
+    };
+
+    //Data
+    const navData = (
+        <>
+            {navLinks.map(navLink => (
+                <a href={navLink.href} key={navLink.name} className="header__nav-item">
+                    {navLink.name}
+                </a>
+            ))}
+        </>
+    );
 
     //HTML structure
-
     return (
-        <div className='header'>
-            <a href="/" className="header__logo-link">
-                <img src={bhLogo} alt="baby hippie logo" className="header__logo" />
-            </a>
-            <div className="header__nav--tab-desk">
-                <a href="/about" className="header__nav-item">about</a>
-                <a href="/services" className="header__nav-item">servies</a>
-                <a href="/blog" className="header__nav-item">blog</a>
+        <>
+            <div className='header'>
+                <div className="header--top">
+                    <a href="/" className="header__logo-link">
+                        <img src={bhLogo} alt="baby hippie logo" className="header__logo" />
+                    </a>
+                    <div className="header__nav--tab-desk">
+                        {navData}
+                    </div>
+                    <div className="header__nav">
+                        <a href="/services" className="header__button">
+                            book appointment
+                        </a>
+                        {/* use state to bring up mobile menu */}
+                        <img
+                            className="header__nav-mbl-icon"
+                            src={iconMblMenu}
+                            alt="hanmburger menu"
+                            onClick={toggleMobileMenu}
+                        />
+                    </div>
+                </div>
+                <div className="header--bottom">
+                    <div className={`header__nav--mbl ${mobileMenuOpen ? 'header__nav--mbl-open' : ''}`}>
+                        {navData}
+                    </div>
+                </div>
             </div>
-            <div className="header__nav">
-                <a href="/services" className="header__button">
-                    book appointment
-                </a>
-
-                {/* use state to bring up mobile menu */}
-                <img src={iconMblMenu} alt="" className="header__nav--mbl" />
-
-            </div>
-        </div>
+        </>
     );
 };
 
